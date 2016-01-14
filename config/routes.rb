ddb1a83e899077fadc2e6760bb6fac1c2bb5bddd
MaksimPW <ma.savichev@gmail.com>
2016-01-14 20:48:45 +0600

    UP root route and test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :words
+  root 'words#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
