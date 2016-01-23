3a36ebcba352b94c21a0ebc2b890a90b5a559152
MaksimPW <ma.savichev@gmail.com>
2016-01-24 02:06:58 +0600

    Change s3_host_name

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,1 @@
-Paperclip::Attachment.default_options[:s3_host_name] = 'img-dictionary.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'img-dictionary.s3-ap-southeast-1.amazonaws.com'
