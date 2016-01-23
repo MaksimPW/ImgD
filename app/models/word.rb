3a3d68e436c13397f9288479aae3ade4cac7e192
MaksimPW <ma.savichev@gmail.com>
2016-01-24 00:33:25 +0600

    Set https protocol in S3

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,4 @@
 class Word < ActiveRecord::Base
-  has_attached_file :img, styles: { medium: "650x400>", thumb: "100x100>" }, default_url: "/imgs/:style/imgD.jpg"
+  has_attached_file :img, :s3_protocol => :https, styles: { medium: "650x400>", thumb: "100x100>" }, default_url: "/imgs/:style/imgD.jpg"
   validates_attachment_content_type :img, content_type: /\Aimage\/.*\Z/
 end
